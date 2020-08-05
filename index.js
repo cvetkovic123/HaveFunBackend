4022e3bdf46fc93a4a8e2915aa1ea4df2bbd6f19
Bojan Cvetkovic <bojan.cvetkovic337@gmail.com>
2020-08-05 12:57:17 +0200

    Added procfile for heroku host, crossedfingers

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 const app = require('./server/app');
-const port = process.env.PORT | 3300;
+const port = process.env.PORT || 3400;
 
 app.listen(port, () => { console.log(`Listening to port ${port}`)}); 
